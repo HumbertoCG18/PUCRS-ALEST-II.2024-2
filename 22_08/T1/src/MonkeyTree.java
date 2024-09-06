3be04e7f2954ee91f803bd5c231b77db5106b0f8
Humberto Corrêa Gomes <humberto.cgomes17@gmail.com>
2024-09-06 00:40:55 -0300

    new layout

--- 22_08/T1/src/MonkeyTree.java
+++ 22_08/T1/src/MonkeyTree.java
@@ -1,6 +1,5 @@
-// Tem que arrumar novamente o algoritimo de exploração, pois ele está mudando de direção mesmo sem um W ou V, uma possivel solução é que, ele só pode mudar de direção caso encontre um W ou um V. O problema que está acontecendo, é que, quando tem o ponteiro está em um número, e ele encontra outro numero a direita ou a esquerda, ele está mudando de direção para aonde esse numero está 
-// Fazer com que as informações que estão sendo atualizadas, fiquem mostrando no painel aonde tem os botões, na esquerda, ao invés da area de renderização
-// Fazer um checkbox, que, caso marcado, a "camera" siga o ponteiro enquanto ela se move 
+// Arrumar o algoritimo, pois ele está mudando de direção no meio do caminho no caso60
+
 package src;
 
 import javax.swing.*;
@@ -19,8 +18,7 @@ public class MonkeyTree extends JPanel {
     private char[][] tree;
     private int[][] path;
     private static int CELL_SIZE = 20; // Tornado variável para controle de zoom
-    private String statusMessage = "Soma atual: 0 | Soma máxima atual: 0";
-    private String treeStatus = "Em Análise";
+    private String treeStatus = "Em Analise";
     private int height, width;
     private int[][] maxPathSums;
     private int maxSum;
@@ -36,6 +34,10 @@ public class MonkeyTree extends JPanel {
     private boolean isReturning = false;
     private boolean isPaused = false;
 
+    // Novo componente para acompanhar a câmera
+    private JCheckBox followPointerCheckBox;
+    private JLabel statusLabel;  // Label para mostrar o status atualizado
+
     public MonkeyTree(String filePath) {
         this.tree = readTreeFromFile(filePath);
         this.path = new int[tree.length][tree[0].length];
@@ -49,6 +51,10 @@ public class MonkeyTree extends JPanel {
 
         setPreferredSize(new Dimension(tree[0].length * CELL_SIZE, tree.length * CELL_SIZE));
         setBackground(Color.BLACK);
+
+        // Inicializa o checkbox e a label de status
+        followPointerCheckBox = new JCheckBox("Seguir Ponteiro");
+        statusLabel = new JLabel("Soma atual: 0 | Soma maxima atual: 0 | Status da arvore: Em Analise");
     }
 
     public static void main(String[] args) {
@@ -61,23 +67,32 @@ public class MonkeyTree extends JPanel {
                 JScrollPane scrollPane = new JScrollPane(visualizer);
                 frame.add(scrollPane);
 
-                JPanel panel = new JPanel();
+                JPanel panel = new JPanel(new BorderLayout());
+                
+                // Criando um sub-painel para os botões e o status
+                JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
                 JButton resetButton = new JButton("Resetar");
                 JButton pauseResumeButton = new JButton("Pausar");
                 JButton centerButton = new JButton("Centralizar no Ponteiro");
                 JButton zoomInButton = new JButton("+");
                 JButton zoomOutButton = new JButton("-");
+                
+                buttonPanel.add(resetButton);
+                buttonPanel.add(pauseResumeButton);
+                buttonPanel.add(centerButton);
+                buttonPanel.add(zoomInButton);
+                buttonPanel.add(zoomOutButton);
+                
+                // Painel para a esquerda onde aparecerá o status e o checkbox
+                JPanel statusPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+                statusPanel.add(visualizer.followPointerCheckBox);  // Adiciona o checkbox
+                statusPanel.add(visualizer.statusLabel);  // Adiciona a label de status
 
-                resetButton.addActionListener(e -> visualizer.resetVisualization(selectedFile));
-                pauseResumeButton.addActionListener(e -> visualizer.togglePauseResume(pauseResumeButton));
-                centerButton.addActionListener(e -> visualizer.centerOnPointer(scrollPane));
+                // Adiciona o painel de status à esquerda e os botões no centro
+                panel.add(statusPanel, BorderLayout.WEST);
+                panel.add(buttonPanel, BorderLayout.CENTER);
 
-                panel.add(resetButton);
-                panel.add(pauseResumeButton);
-                panel.add(centerButton);
                 frame.add(panel, BorderLayout.SOUTH);
-                panel.add(zoomInButton);
-                panel.add(zoomOutButton);
 
                 frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -85,17 +100,22 @@ public class MonkeyTree extends JPanel {
 
                 visualizer.startVisualization();
 
-                                // Ações de zoom
-                                zoomInButton.addActionListener(e -> {
-                                    visualizer.zoomIn();
-                                    visualizer.revalidate();
-                                    visualizer.repaint();
-                                });
-                                zoomOutButton.addActionListener(e -> {
-                                    visualizer.zoomOut();
-                                    visualizer.revalidate();
-                                    visualizer.repaint();
-                                });
+                // Ações de zoom
+                zoomInButton.addActionListener(e -> {
+                    visualizer.zoomIn();
+                    visualizer.revalidate();
+                    visualizer.repaint();
+                });
+                zoomOutButton.addActionListener(e -> {
+                    visualizer.zoomOut();
+                    visualizer.revalidate();
+                    visualizer.repaint();
+                });
+                
+                // Botões de controle
+                resetButton.addActionListener(e -> visualizer.resetVisualization(selectedFile));
+                pauseResumeButton.addActionListener(e -> visualizer.togglePauseResume(pauseResumeButton));
+                centerButton.addActionListener(e -> visualizer.centerOnPointer(scrollPane));
             }
         });
     }
@@ -112,7 +132,7 @@ public class MonkeyTree extends JPanel {
                 selectedFile = (String) JOptionPane.showInputDialog(
                         null,
                         "Escolha um arquivo TXT para visualizar:",
-                        "Seleção de Arquivo",
+                        "Selecao de Arquivo",
                         JOptionPane.PLAIN_MESSAGE,
                         null,
                         txtFiles,
@@ -154,7 +174,7 @@ public class MonkeyTree extends JPanel {
         }
     }
 
-    public void startVisualization() {
+     public void startVisualization() {
         resetState();
         findStartingPoint();
         calculateMaxPath();
@@ -192,28 +212,34 @@ public class MonkeyTree extends JPanel {
         }
     }
 
+    public void zoomIn() {
+        CELL_SIZE += 5;
+        setPreferredSize(new Dimension(width * CELL_SIZE, height * CELL_SIZE));
+    }
+
+    public void zoomOut() {
+        if (CELL_SIZE > 5) {
+            CELL_SIZE -= 5;
+            setPreferredSize(new Dimension(width * CELL_SIZE, height * CELL_SIZE));
+        }
+    }
+
     private void resetState() {
         this.path = new int[height][width];
         this.currentSum = 0;
         this.maxSum = 0;
-        this.treeStatus = "Em Análise";
+        this.treeStatus = "Em Analise";
         dfsStack.clear();
         maxPathStack.clear();
         isReturning = false;
         isPaused = false;
     }
 
-    private void findStartingPoint() {
-        for (int col = 0; col < width; col++) {
-            if (tree[height - 1][col] == '|') {
-                startRow = height - 1;
-                startCol = col;
-                currentRow = startRow;
-                currentCol = startCol;
-                return;
-            }
-        }
+    // Atualizando a label de status
+    private void updateStatusMessage() {
+        statusLabel.setText(String.format("Soma atual: %d | Soma maxima atual: %d | Status da arvore: %s", currentSum, maxSum, treeStatus));
     }
+
 
     private void calculateMaxPath() {
         maxPathSums = new int[height][width];
@@ -256,7 +282,7 @@ public class MonkeyTree extends JPanel {
             int depth = pos[2];
 
             if (isReturning) {
-                treeStatus = "Em Análise";  // Muda o status para "Em Análise" durante o retorno
+                treeStatus = "Em Analise"; // Muda o status para "Em Análise" durante o retorno
 
                 if (tree[currentRow][currentCol] == 'V') {
                     if (canMove(currentRow, currentCol, "left")) {
@@ -275,7 +301,7 @@ public class MonkeyTree extends JPanel {
                     } else if (canMove(currentRow, currentCol, "straight")) {
                         isReturning = false;
                         move(currentRow, currentCol, depth, "straight");
-                    }                     else if (canMove(currentRow, currentCol, "right")) {
+                    } else if (canMove(currentRow, currentCol, "right")) {
                         isReturning = false;
                         move(currentRow, currentCol, depth, "right");
                     } else {
@@ -346,42 +372,11 @@ public class MonkeyTree extends JPanel {
                 }
             }
         } else {
-            stepTimer.stop();
             paintMaxPath();
-            treeStatus = "Completo";
-            JOptionPane.showMessageDialog(this, "Soma máxima do caminho: " + maxSum);
         }
-
-        statusMessage = String.format("Soma atual: %d | Soma máxima atual: %d", currentSum, maxSum);
+        updateStatusMessage();
     }
-
-    private void processReturning() {
-        if (Character.isDigit(tree[currentRow][currentCol])) {
-            int value = Character.getNumericValue(tree[currentRow][currentCol]);
-            if (value > 0 && currentSum > 0) {  // Evitar que a soma vá para valores negativos
-                currentSum -= value;
-            }
-        }
-        path[currentRow][currentCol] = 2;
-        dfsStack.pop();
-        repaint();
-    }
-
-    private void saveMaxPath() {
-        maxPathStack.clear();
-        for (int[] position : dfsStack) {
-            maxPathStack.push(new int[]{position[0], position[1]});
-        }
-    }
-
-    private void paintMaxPath() {
-        while (!maxPathStack.isEmpty()) {
-            int[] pos = maxPathStack.pop();
-            path[pos[0]][pos[1]] = 3;
-        }
-        repaint();
-    }
-
+    
     private boolean canMove(int row, int col, String direction) {
         int newRow = row - 1;
         int newCol = col;
@@ -432,6 +427,46 @@ public class MonkeyTree extends JPanel {
         return row >= 0 && col >= 0 && col < width && tree[row][col] != ' ';
     }
 
+    private void findStartingPoint() {
+        for (int col = 0; col < width; col++) {
+            if (tree[height - 1][col] == '|') {
+                startRow = height - 1;
+                startCol = col;
+                currentRow = startRow;
+                currentCol = startCol;
+                return;
+            }
+        }
+    }
+
+    private void processReturning() {
+        if (Character.isDigit(tree[currentRow][currentCol])) {
+            int value = Character.getNumericValue(tree[currentRow][currentCol]);
+            if (value > 0 && currentSum > 0) { // Evitar que a soma vá para valores negativos
+                currentSum -= value;
+            }
+        }
+        path[currentRow][currentCol] = 2;
+        dfsStack.pop();
+        repaint();
+    }
+    
+    private void saveMaxPath() {
+        maxPathStack.clear();
+        for (int[] position : dfsStack) {
+            maxPathStack.push(new int[]{position[0], position[1]});
+        }
+    }
+
+    private void paintMaxPath() {
+        while (!maxPathStack.isEmpty()) {
+            int[] pos = maxPathStack.pop();
+            path[pos[0]][pos[1]] = 3;
+        }
+        repaint();
+    }
+
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -464,18 +499,5 @@ public class MonkeyTree extends JPanel {
         }
 
         g.setColor(Color.WHITE);
-        g.drawString(statusMessage + " | Status da árvore: " + treeStatus, 10, getHeight() - 10);
-    }
-
-    public void zoomIn() {
-        CELL_SIZE += 5;
-        setPreferredSize(new Dimension(width * CELL_SIZE, height * CELL_SIZE));
-    }
-
-    public void zoomOut() {
-        if (CELL_SIZE > 5) {
-            CELL_SIZE -= 5;
-            setPreferredSize(new Dimension(width * CELL_SIZE, height * CELL_SIZE));
-        }
     }
 }
